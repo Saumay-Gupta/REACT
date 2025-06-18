3436d40bcd7102b479e301dc5d4f0bea799f8616
Saumay Gupta <saumaygupta03@gmail.com>
2025-06-18 16:01:12 +0530

    currenyConvertor

--- currencyConvertor/src/App.jsx
+++ currencyConvertor/src/App.jsx
@@ -1,18 +1,48 @@
 import { useState } from 'react'
 import './App.css'
 import Box from './components/Box'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
 
   return (
     <>
       <div className='h-screen w-full flex justify-center' style={{backgroundColor: "#212223"}}>
-      <div className='h-1/2 w-1/3 my-20 rounded-2xl flex flex-col justify-center relative' style={{backgroundColor: "#464f4d"}}>
-        <button className='text-white bg-blue-600 p-2 w-1/7 mx-54 absolute z-10 rounded-2xl'>swap</button>
-        <Box user="From" currency="usd"/>
-        <Box user="To" currency="inr"/>
-      </div>
+        <div className='h-6/10 w-1/3  rounded-2xl flex flex-col justify-center ' style={{backgroundColor: "#464f4d"}}>
+          <form onSubmit={(e) => {e.preventDefault(); convert()}} className='flex flex-col items-center justify-center'>
+              <Box label="From" amount={amount} currencyOptions={options} onCurrencyChange={(currency) => setAmount(amount)}
+              selectCurrency={from}
+              onAmountChange={(amount) => setAmount(amount)}
+                />
+              <button onClick={swap} className='text-white bg-blue-600 p-2 w-1/7 mx-54 absolute z-10 rounded-2xl'>swap</button>
+            <Box label="To" amount={convertedAmount} currencyOptions={options} onCurrencyChange={(currency) => setTo(currency)} selectCurrency={to }/>
+            <div className="mt-auto flex justify-center mb-3">
+              <button type='submit' className='bg-blue-400 text-white rounded-xl px-4 py-2'>
+                Convert {from.toUpperCase()} to {to.toUpperCase()}
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   )
--- currencyConvertor/src/components/Box.jsx
+++ currencyConvertor/src/components/Box.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 
-function Box(props){
+function Box({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    // amountDisable = false,
+    // currencyDisable = false,
+}){
     return(
         <>
             <div className='bg-white h-3/10 w-8/10 my-2 mx-12 rounded-xl flex'>
-                <div>
-                <p className='text-gray-600 mx-3 my-1'>{props.user}</p>
-                <input type="text" defaultValue={0} className='mx-3 my-10'/>
+
+                <div className='h-10/10 flex flex-col'>
+                    <p className='text-gray-600 mx-3 my-1'>{label}</p>
+                    <input type="number" className='mx-3  mt-auto mb-3 ' value={amount}
+                        onChange={(e) => onAmountChange && onAmountChange(e.target.value)}
+                    />
                 </div>
-                <div className='h-1/2 w-5/10 mx-3'>
-                <p className='text-gray-600 mx-10 my-1'>Currency Type</p>
-                <select name="currency" id="currency" defaultValue={props.currency} className='my-9 mx-15 bg-gray-300 rounded-xl w-21 h-8'>
-                    <option value="inr">inr</option>
-                    <option value="usd">usd</option>
-                </select>
+
+                <div className='h-1/2 w-5/10 flex flex-col items-end mr-4'>
+                    <p className='text-gray-600 my-1'>Currency Type</p>
+                    <select name="currency" id="currency" className='my-9  bg-gray-300 rounded-xl w-21 h-8'
+                        value ={selectCurrency} 
+                        onChange = {(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    >
+                        {currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>
+                                {currency}
+                            </option>
+                        ))}
+                    </select>
                 </div>
             </div>
         </>
